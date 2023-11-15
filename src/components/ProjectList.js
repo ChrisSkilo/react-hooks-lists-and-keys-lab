c7ff06e73560511127e0f3c2a2e5df682790ba64
Chrispus <skilochris@gmail.com>
2023-11-15 08:05:04 +0300

    completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,13 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map(project => (
+          <ProjectItem key={project.id} name={project.name} technologies={project.technologies} about={project.about} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default ProjectList;
